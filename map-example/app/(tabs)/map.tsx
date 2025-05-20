a408235a1444e4e293bf36559337064705bf31a8
Charlie Cheever <ccheever@gmail.com>
2025-05-20 11:23:12 -0700

    Map example

--- map-example/app/(tabs)/map.tsx
+++ map-example/app/(tabs)/map.tsx
@@ -1,19 +1,61 @@
-import { ThemedView } from "@/components/ThemedView";
-import { StyleSheet, Text } from "react-native";
+import * as Location from 'expo-location';
+import { AppleMaps, GoogleMaps } from "expo-maps";
+import { useEffect, useState } from "react";
+import { Platform, StyleSheet, Text } from "react-native";
 
 export default function MapScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <Text style={{ color: "#ffcc00" }}>This is the map screen</Text>
-    </ThemedView>
-  );
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if (errorMsg) {
+    return <Text>{errorMsg}</Text>;
+  }
+
+  if (Platform.OS === "ios") {
+    return (
+      <AppleMaps.View 
+        style={{ flex: 1 }}
+        cameraPosition={location ? {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          zoom: 15
+        } : undefined}
+      />
+    );
+  } else if (Platform.OS === "android") {
+    return (
+      <GoogleMaps.View 
+        style={{ flex: 1 }}
+        cameraPosition={location ? {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          zoom: 15
+        } : undefined}
+      />
+    );
+  } else {
+    return <Text>Maps are only available on Android and iOS</Text>;
+  }
 }
 
-const styles = StyleSheet.create({
+let styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   map: {
     width: "100%",
--- map-example/app/_layout.tsx
+++ map-example/app/_layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack initialRouteName="(tabs)/map">
+      <Stack >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
